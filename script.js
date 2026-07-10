4c590cc3c9cb56dbc8b5c080beea1b44bb7823bc
mezge002 <mezge002@umn.edu>
2026-07-10 14:51:58 -0500

    fixed api url, again

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const VISITOR_API_URL = 'https://${{ secrets.AWS_API_ID }}.execute-api.us-east-1.amazonaws.com/resume/visitors';
+const VISITOR_API_URL = 'https://t9czi3e9s2.execute-api.us-east-1.amazonaws.com/resume/visitors';
 
 fetch(VISITOR_API_URL)
   .then(res => res.json())
   .then(data => {
-    document.getElementById('visitor-count').textContent = data.count;
+    console.log(data);
+    document.getElementById('visitor-count').textContent = data.body;
   })
-  .catch(() => {
+  .catch((error) => {
     document.getElementById('visitor-count').textContent = 'n/a';
+    console.log(error)
   });
